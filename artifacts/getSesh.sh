6e537c3600d2049d413d160ff9b13f94776b4909
johnny-r3v Soffe <jrsoffe@churchofjesuschrist.org>
2022-11-07 08:54:26 -0700

    fixed formatting in getSesh

--- artifacts/getSesh.sh
+++ artifacts/getSesh.sh
@@ -8,6 +8,6 @@ mdrole=$(curl http://169.254.169.254/latest/meta-data/iam/security-credentials/)
 # request to get the session token for the given role. 
 curl http://169.254.169.254/latest/meta-data/iam/security-credentials/$mdrole --output ./ics-decon-s3-creds.json
 
-echo "[i] Creds are ready for $mdrole, look for ics-decon-s3-creds.json in your $CWD"
-echo "[!] Use them wisely.. great power vs. responsibility and all that. 
+echo "[i] Creds are ready for $mdrole, look for ics-decon-s3-creds.json in your $(pwd) directory."
+echo "[!] Use them wisely.. great power vs. responsibility and all that."
 
